b6797ca47bd312b5b276105d59acd1edd47bf5a4
Miguel Arcanjo <mig.ramos10@gmail.com>
2025-03-18 22:57:14 -0300

    feat: create menu

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import "./styles/global.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home/index.tsx";
 import { About } from "./components/About/index.tsx";
+import { Menu } from "./components/Menu/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+      <Menu />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
